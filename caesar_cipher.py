809823a030abe832ca2e4eebf166f9e843a1e4fc
ConsciousCoder1 <agpperez1@gmail.com>
2021-06-11 20:22:51 -0400

    Minor tweaks to module

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,20 +1,10 @@
-print('''
-    ______      _____                  _   _             
-    | ___ \    /  __ \                | | (_)            
-    | |_/ /   _| /  \/_ __ _   _ _ __ | |_ _  ___  _ __  
-    |  __/ | | | |   | '__| | | | '_ \| __| |/ _ \| '_ \ 
-    | |  | |_| | \__/\ |  | |_| | |_) | |_| | (_) | | | |
-    \_|   \__, |\____/_|   \__, | .__/ \__|_|\___/|_| |_|
-        __/ |            __/ | |                      
-        |___/            |___/|_|                      ''')
-print('\n--------------------- CAESAR STYLE ---------------------\n')
-print("\nWelcome to PyCrytion -- Caeser Style!\n")
+from logo import logoprint
 
+logoprint()
 
 encode_decode = input("\nTo encrypt a message, type 'encode'. To decrypt a message, type 'decode'. --> ")
 user_message = list(input("Please enter your message --> "))
 cipher_number = int(input("How many places will you cipher shift? --> "))
-
 
 def encode(user_message=user_message, cipher_number=cipher_number):
     from logo import alphabet
@@ -27,7 +17,7 @@ def encode(user_message=user_message, cipher_number=cipher_number):
             print(' ')
     print(f"Your encrypted message:\n{user_message_encoded}")
     
-
+from logo import alphabet
 def decode(user_message = user_message, cipher_number = cipher_number):
     user_message_decoded = ""
     for i in user_message:
